b712732965453588f049970eace93b58a16ee82c
basanya ajibola <albajd@gmail.com>
2023-09-09 01:39:44 +0100

    files

--- utils/db.js
+++ utils/db.js
@@ -13,25 +13,3 @@ class DBClient {
       this.db = this.client.db(`${DATABASE}`);
     }).catch((err) => {
       console.log(err);
-    });
-  }
-
-  isAlive() {
-    return this.client.isConnected();
-  }
-
-  async nbUsers() {
-    const users = this.db.collection('users');
-    const usersNum = await users.countDocuments();
-    return usersNum;
-  }
-
-  async nbFiles() {
-    const files = this.db.collection('files');
-    const filesNum = await files.countDocuments();
-    return filesNum;
-  }
-}
-
-const dbClient = new DBClient();
-module.exports = dbClient;
